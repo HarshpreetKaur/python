e167ba522a36497420f2ecae144e5142c299d27d
HarshpreetKaur <harshahuja198@gmail.com>
2017-07-11 00:06:44 -0400

    variables

---
+++ variables.py
@@ -0,0 +1,15 @@
+manila_pop = 1780148
+manila_area = 16.56
+manila_pop_density = manila_pop / manila_area
+print(manila_pop_density)
+
+reservoir_volume = 4.445e8
+rainfall = 5e6
+rainfall *= .9
+reservoir_volume += rainfall
+reservoir_volume *= 1.05
+reservoir_volume *= 0.95
+reservoir_volume -= 2.5e5 
+print(reservoir_volume)
+
+savings, salary = 514.86, 320.51
