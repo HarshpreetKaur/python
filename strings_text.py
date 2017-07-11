dd74c8cd49be8da62612a01364167e8ccf4df4ac
HarshpreetKaur <harshahuja198@gmail.com>
2017-07-11 00:38:24 -0400

    Strings and text

---
+++ strings_text.py
@@ -0,0 +1,24 @@
+print("hello")
+print ('hello')
+
+welcome_message = "Hello, welcome to my Repository!"
+print(welcome_message)
+
+instructor_1 = "Philip"
+instructor_2 = "Charlie"
+print(instructor_1 + instructor_2)
+print(instructor_1 + " and " + instructor_2)
+
+pet_halibut = 'Why should I be tarred with the epithet "loony" merely because I have a pet halibut?'
+salesman = '"I think you\'re an encyclopaedia salesman"'
+
+coconut_count = "34"
+mango_count = "15"
+tropical_fruit_count = coconut_count + mango_count
+print(tropical_fruit_count)
+
+username = "Kinari"
+timestamp = "04:50"
+url = "http://petshop.com/pets/mammals/cats"
+#log message
+print(username + " accessed the site " + url + " at " + timestamp + ".")
