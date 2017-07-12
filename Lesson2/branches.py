4f2caaf0d922c695b244c8cdc0ea03d6a3dec1e9
HarshpreetKaur <harshahuja198@gmail.com>
2017-07-12 10:37:37 -0400

    branches

---
+++ Lesson2/branches.py
@@ -0,0 +1,56 @@
+phone_balance = 5
+bank_balance = 300
+if phone_balance < 10:
+   phone_balance += 10
+   bank_balance -= 10
+print(phone_balance)
+print(bank_balance)
+
+weight_in_kg = 60
+height_in_m = 1.69164
+if 18.5 <= weight_in_kg / (height_in_m)**2 < 25:
+	print("BMI is considered 'normal'.")
+
+number = 5567876543
+if number % 2 == 0:
+    print("The number " + str(number) + " is even.")
+else:
+    print("The number " + str(number) + " is odd.")
+
+
+def garden_calendar(season):
+    if season == "spring":
+        print("time to plant the garden!")
+    elif season == "summer":
+        print("time to water the garden!")
+    elif season == "autumn" or season == "fall":
+        print("time to harvest the garden!")
+    elif season == "winter":
+        print("time to stay indoors and drink tea!")
+    else:
+        print("I don't recognize that season")
+
+garden_calendar("spring")
+
+
+def which_prize(points):
+    """
+    Returns the prize
+    """
+    if points <= 50:
+        return "Congratulations! You have won a wooden rabbit!"
+    elif points <= 150:
+        return "Oh dear, no prize this time."
+    elif points <= 180:
+        return "Congratulations! You have won a wafer-thin mint!"
+    else:
+        return "Congratulations! You have won a penguin!"
+
+print(which_prize(123))
+
+
+is_raining = True
+is_sunny = False
+if is_raining and is_sunny:
+    print("Is there a rainbow?")
+
