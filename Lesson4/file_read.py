6bd5432456691a02ac861bff4a11bfcc38414c86
harshpreetkaur <Outreachy@MacBook.local>
2017-08-13 22:29:35 -0400

    default arguments, file read, libraries

---
+++ Lesson4/file_read.py
@@ -0,0 +1,11 @@
+def create_cast_list(filename):
+    cast_list = []
+    #use with to open the file filename
+    #use the for loop syntax to process each line
+    #and add the actor name to cast_list
+    with open(filename) as f:
+        for line in f:
+            a = line.split(",",1)
+            cast_list.append(a[0])
+
+    return cast_list
---
+++ Lesson4/library.py
@@ -0,0 +1,21 @@
+# Use an import statement at the top
+import random 
+
+word_file = "words.txt"
+word_list = []
+
+#fill up the word_list
+with open(word_file,'r') as words:
+	for line in words:
+		# remove white space and make everything lowercase
+		word = line.strip().lower()
+		# don't include words that are too long or too short
+		if 3 < len(word) < 8:
+			word_list.append(word)
+
+# Add your function here
+# It should return a string consisting of three random words 
+# concatenated together without spaces
+def generate_password():
+    a = word_list[random.randint(0, len(word_list)-1)] + word_list[random.randint(0, len(word_list)-1)] + word_list[random.randint(0, len(word_list)-1)]
+    return a
